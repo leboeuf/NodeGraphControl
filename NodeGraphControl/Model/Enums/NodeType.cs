070dedcfe043a0bed5c2572df34ac5f6666b55aa
Maxime Leboeuf <ml@maximeleboeuf.com>
2016-01-18 17:22:29 -0500

    Add NodeType Enum

---
+++ NodeGraphControl/Model/Enums/NodeType.cs
@@ -0,0 +1,13 @@
+﻿namespace NodeGraphControl.Model.Enums
+{
+    /// <summary>
+    /// Represents the type of a node.
+    /// </summary>
+    public enum NodeType
+    {
+        Standard,
+        Custom,
+        Input,
+        Output
+    }
+}
